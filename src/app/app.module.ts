5c9046ded6cd30cb4a7e15cbe6857f608766b682
allon <allonloveapple@gmail.com>
2018-08-27 15:18:16 +1000

    update tabletype

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,8 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MyDatePickerModule} from 'mydatepicker';
 import { HeadBannerComponent } from './head-banner/head-banner.component';
 import { TableMapComponent } from './table-map/table-map.component';
-import { SquareTableComponent } from './square-table/square-table.component';
-import { CircleTableComponent } from './circle-table/circle-table.component';
+import { SquareTableComponent } from './tableType/square-table/square-table.component';
+import { CircleTableComponent } from './tableType/circle-table/circle-table.component';
 
 @NgModule({
   declarations: [
